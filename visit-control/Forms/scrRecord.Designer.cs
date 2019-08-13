9da69f8cc9486c78f63dbcb5307f298af24c301b
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2019-08-13 12:58:43 -0500

    Record Screen

---
+++ visit-control/Forms/scrRecord.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace visit_control.Forms
+{
+    partial class scrRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(scrRecord));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tbSearchValue = new System.Windows.Forms.TextBox();
+            this.dtpF1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpF2 = new System.Windows.Forms.DateTimePicker();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvVisit = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbSearchValue
+            // 
+            this.tbSearchValue.Location = new System.Drawing.Point(12, 72);
+            this.tbSearchValue.Name = "tbSearchValue";
+            this.tbSearchValue.Size = new System.Drawing.Size(321, 20);
+            this.tbSearchValue.TabIndex = 0;
+            // 
+            // dtpF1
+            // 
+            this.dtpF1.CustomFormat = "dd/MMMM/yyyy";
+            this.dtpF1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpF1.Location = new System.Drawing.Point(349, 72);
+            this.dtpF1.Name = "dtpF1";
+            this.dtpF1.Size = new System.Drawing.Size(137, 20);
+            this.dtpF1.TabIndex = 1;
+            // 
+            // dtpF2
+            // 
+            this.dtpF2.CustomFormat = "dd/MMMM/yyyy";
+            this.dtpF2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpF2.Location = new System.Drawing.Point(492, 72);
+            this.dtpF2.Name = "dtpF2";
+            this.dtpF2.Size = new System.Drawing.Size(137, 20);
+            this.dtpF2.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSearch.BackgroundImage")));
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Location = new System.Drawing.Point(635, 72);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(22, 20);
+            this.btnSearch.TabIndex = 18;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvVisit
+            // 
+            this.dgvVisit.AllowUserToAddRows = false;
+            this.dgvVisit.AllowUserToDeleteRows = false;
+            this.dgvVisit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvVisit.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvVisit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVisit.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column5,
+            this.Column7,
+            this.Column2,
+            this.Column3});
+            this.dgvVisit.Location = new System.Drawing.Point(12, 107);
+            this.dgvVisit.Name = "dgvVisit";
+            this.dgvVisit.ReadOnly = true;
+            this.dgvVisit.Size = new System.Drawing.Size(776, 331);
+            this.dgvVisit.TabIndex = 53;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "NOMBRE";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "APELLIDO PATERNO";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 150;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "APELLIDO MATERNO";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "DEPARTAMENTO";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle1.Format = "G";
+            dataGridViewCellStyle1.NullValue = null;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column3.HeaderText = "FECHA DE ENTRADA";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 200;
+            // 
+            // scrRecord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvVisit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dtpF2);
+            this.Controls.Add(this.dtpF1);
+            this.Controls.Add(this.tbSearchValue);
+            this.Name = "scrRecord";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro";
+            this.Load += new System.EventHandler(this.scrRecord_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSearchValue;
+        private System.Windows.Forms.DateTimePicker dtpF1;
+        private System.Windows.Forms.DateTimePicker dtpF2;
+        internal System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvVisit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
---
+++ visit-control/Forms/scrRecord.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using visit_control.Connection;
+
+namespace visit_control.Forms
+{
+    public partial class scrRecord : MaterialForm
+
+    {
+        public scrRecord()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Red900, //Color dialog 
+                Primary.Red900, //Color control buttons
+                Primary.Blue400,
+                Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+        #region FUNCIONES
+
+        private void fill_dgv(string searchValue, DateTime date1, DateTime date2)
+        {
+            dgvVisit.Rows.Clear();
+            var db = new ConnectionDB();
+
+            var data = db.Visitors.Join(db.Visitors_Department, x => x.id, y => y.id_visitor, (x, y) => new
+            {
+                x.id,
+                x.name,
+                x.last_name,
+                x.m_last_name,
+                x.status,
+                y.department,
+                y.entry
+            }).Where(x=> x.status == 1 && x.entry >= DbFunctions.TruncateTime(date1) && x.entry <= DbFunctions.TruncateTime(date2));
+
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                data = data.Where(x => x.name.Contains(searchValue) ||
+                                       x.last_name.Contains(searchValue) ||
+                                       x.m_last_name.Contains(searchValue) ||
+                                       x.department.Contains(searchValue)
+                );
+            }
+
+            foreach (var item in data.ToList())
+            {
+                dgvVisit.Rows.Add(item.name,
+                    item.last_name,
+                    item.m_last_name,
+                    item.department,
+                    item.entry);
+            }
+        }
+
+
+        #endregion
+
+        private void scrRecord_Load(object sender, EventArgs e)
+        {
+            fill_dgv(tbSearchValue.Text,dtpF1.Value,dtpF2.Value);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            fill_dgv(tbSearchValue.Text, dtpF1.Value, dtpF2.Value);
+        }
+    }
+}
