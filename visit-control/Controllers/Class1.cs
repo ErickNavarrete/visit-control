d581b134c031745f4edcb9890c42e1c5de9084f2
Alejandro <alexrodriguezserrano1595@gmail.com>
2019-08-13 12:57:56 -0500

    add controller camera

--- visit-control/Controllers/Class1.cs
+++ visit-control/Controllers/Class1.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 namespace visit_control.Controllers
 {
+
     class camera
     {
 
+        private camera()
+        {
+            Console.WriteLine("Iniciando camara");
+        }
+
+        VideoCapture videoCapture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        bool isCameraRunning;
+
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+        }
     }
 }
---
+++ visit-control/Forms/scrCapturePhoto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+namespace visit_control.Forms
+{
+    public partial class scrCapturePhoto : MaterialForm
+    {
+       
+            
+        
+        public scrCapturePhoto()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Red900, //Color dialog 
+                Primary.Red900, //Color control buttons
+                Primary.Blue400,
+                Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+    
+    }
+}
